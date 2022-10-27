d9d36f01fc283fb34abfe4b85136c9d199977c57
Erik Villarreal <erikvillarreal7@gmail.com>
2022-10-28 00:25:51 +0200

    fix for windows

--- libs/bullet.lua
+++ libs/bullet.lua
@@ -11,9 +11,17 @@ project "bullet"
 	files
 	{
         "bullet/src/*",
+		"bullet/src/LinearMath/**",
+		"bullet/src/BulletCollision/**",
+		"bullet/src/BulletDynamics/**",
+		"bullet/src/BulletInverseDynamics/**",
+		"bullet/src/BulletSoftBody/**",
         "bullet/src/Bullet3Common/**",
 		"bullet/src/Bullet3Collision/**",
 		"bullet/src/Bullet3Geometry/**",
+		-- "bullet/src/Bullet3OpenCL/**",
+		"bullet/src/Bullet3Serialize/**",
+		"bullet/src/clew/**",
 	}
     
 	filter "system:linux"
@@ -32,7 +40,11 @@ project "bullet"
 		staticruntime "On"
 
 		defines 
-		{ 
+		{
+			"BT_USE_SSE_IN_API",	-- this is disabled in windows by default
+									-- adds a bit of performance gain if used correctly
+									-- library says its harder to integrate under windows with this
+									-- library does not compile unless this is enabled :D
 			"_BULLET_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
